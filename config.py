09fba3d7356bdccf5c98db82b7146e09812fe049
Micik2 <micik220@gmail.com>
2021-11-26 18:39:18 +0100

    Heroku update and hide configuration values

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ Project configuration file
 
 import secrets
 
-postgres_config = dict(user='postgres',
-                       password='',
-                       db='library',
-                       host='localhost',
+postgres_config = dict(user='hidden',
+                       password='hidden',
+                       db='hidden',
+                       host='hidden',
                        port=5432)
-postgres_config_test = dict(user='postgres',
-                            password='',
-                            db='library_Test',
-                            host='localhost',
+postgres_config_test = dict(user='hidden',
+                            password='hidden',
+                            db='hidden',
+                            host='hidden',
                             port=5432)
 secret_key = secrets.token_urlsafe(16)
 BOOKS_URL = 'https://www.googleapis.com/books/v1/volumes?q=Hobbit'
